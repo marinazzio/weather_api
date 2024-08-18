a7fb98cd960a6ad08e6c6b75e23b64f546975f1a
Denis Kiselev <denis.kiselyov@gmail.com>
2024-08-18 17:08:39 +0700

    Implement by_time endpoint (#30)

---
+++ spec/api/weather/v1/by_time_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe Weather::V1::ByTime do
+  context "GET /api/weather/v1/by_time" do
+    let(:timestamp) { 1621823790 }
+
+    context "without matching measures" do
+      it "returns 404" do
+        get "/api/weather/v1/by_time?timestamp=#{timestamp}"
+
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context "with matching measures" do
+      before do
+        create(:measure, timestamp: "2021-05-24T08:00:00+03:00", temperature: 1)
+      end
+
+      it "returns the closest measure by passed time in Unix format" do
+        get "/api/weather/v1/by_time?timestamp=#{timestamp}"
+
+        expect(response).to have_http_status(200)
+        expect(response.body).to eq({ temperature: "1.0" }.to_json)
+      end
+    end
+  end
+end
